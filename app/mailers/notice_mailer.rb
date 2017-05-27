32620ae8f291403dd456ffd0e4acd83cbb5fcb99
Yasuhiro <johndoe@example.com>
2017-05-27 06:31:39 +0000

    MESSAGE

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,14 +1,13 @@
 class NoticeMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notice_mailer.sendmail_topics.subject
   #
-  def sendmail_blog(blog)
-    @topics = topic
-
-    mail to: "banibanibani50@gmail.com"
-    subject: '【Topics】トピックが投稿されました'
+  def sendmail_topic(topic)
+    @topic = topic
+    mail to:"banibanibani50@gmail.com"
+         subject:'【Topics】トピックが投稿されました'
   end
 end
+
