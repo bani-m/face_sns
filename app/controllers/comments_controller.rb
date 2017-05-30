62d9e40495a989edf4621163343280fae51b6a3b
Yasuhiro <johndoe@example.com>
2017-05-30 14:04:47 +0000

    log

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        format.html { redirect_to topic_path(@topic), notice: 'コメントを投稿しました。' }
       else
         format.html { render :new }
       end
