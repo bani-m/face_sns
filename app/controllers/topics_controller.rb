28c1eb300262d33ae4ecccc8ae68d86e72281abb
Yasuhiro <johndoe@example.com>
2017-05-30 10:00:35 +0000

    heroku

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -64,8 +64,7 @@ class TopicsController < ApplicationController
     def topics_params
       params.require(:topic).permit(:title, :content, :user_id, :name)
     end
-   # idをキーとして値を取得するメソッド
+end   # idをキーとして値を取得するメソッド
     def set_topic
       @topic = Topic.find(params[:id])
     end
-end
