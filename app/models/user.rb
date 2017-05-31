e054967fb5579a48a051a4bb65b90a6acea84db2
Yasuhiro <johndoe@example.com>
2017-05-31 13:58:21 +0000

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
   has_many :topics, dependent: :destroy
   # CommentモデルのAssociationを設定
   has_many :comments, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'relationships/create'
+
+  get 'relationships/destroy'
+
 resources :topics, only: [:index, :new, :create]
 
   get 'notifications/index'
