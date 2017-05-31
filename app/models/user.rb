4c241a073dc7bc56bd44bf9dc385bc6d9d8c71ef
Yasuhiro <johndoe@example.com>
2017-05-31 14:59:57 +0000

    following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :followed_users, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_relationships, source: :follower
-  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
-  has_many :topics, dependent: :destroy
-  # CommentモデルのAssociationを設定
-  has_many :comments, dependent: :destroy
+ 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
 
-  mount_uploader :avatar, AvatarUploader      
+  mount_uploader :avatar, AvatarUploader  
+
+  has_many :topics, dependent: :destroy
+  # CommentモデルのAssociationを設定
+  has_many :comments, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower    
 def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.find_by(email: auth.info.email)
 
