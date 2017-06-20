c676b54efc18cdcc68a6fba62bae104d958fd89f
Yasuhiro <johndoe@example.com>
2017-06-20 14:21:33 +0000

    aws

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,30 @@
-100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               uid: n,
-               provider: n,
-               )
-end
+#100.times do |n|
+#  email = Faker::Internet.email
+#  password = "password"
+# User.create!(email: email,
+ #              password: password,
+  #             password_confirmation: password,
+   #            uid: n,
+    #           provider: n,
+     #          )
+#end
 
-100.times do |t|
-  title = Faker::Book.title
-  content = Faker::Beer.name
-  Topic.create!(title: title,
-               content: content,
-               )
+#100.times do |t|
+ # title = Faker::Book.title
+  #content = Faker::Beer.name
+  #Topic.create!(title: title,
+    #           content: content,
+    #          )
 
-end
+#end
 
 100.times do |p|
   comment = Faker::LordOfTheRings.character
-  comment.create!(comment: comment,
-               )
-
+  Comment.create!(user_id: user_id,
+                  topic_id: topic_id,
+                  content: content,
+                  )
 end
+#t.integer  "user_id"
+ #   t.integer  "topic_id"
+  #  t.text     "content"
