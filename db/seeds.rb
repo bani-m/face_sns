41af9b82909903d32ac39b82473d14e1c79c16aa
Yasuhiro <johndoe@example.com>
2017-06-24 14:22:00 +0000

    last

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
-#100.times do |n|
-#  email = Faker::Internet.email
-#  password = "password"
-# User.create!(email: email,
- #              password: password,
-  #             password_confirmation: password,
-   #            uid: n,
-    #           provider: n,
-     #          )
-#end
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+ User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               uid: n,
+               provider: n,
+               )
+end
 
-#100.times do |t|
- # title = Faker::Book.title
-  #content = Faker::Beer.name
-  #Topic.create!(title: title,
-    #           content: content,
-    #          )
+100.times do |t|
+ title = Faker::Book.title
+  content = Faker::Beer.name
+  Topic.create!(title: title,
+               content: content,
+              )
 
-#end
+end
 
 100.times do |p|
   content = Faker::Bank.name
