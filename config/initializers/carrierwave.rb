b90768c328fe6ec2f39b80c83d7d144055fc7229
Apple <apple@Apple-no-MacBook-Pro.local>
2017-07-16 15:21:07 +0900

    test paramter

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      #aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      #aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_access_key_id:     1,
+      aws_secret_access_key: 3,
       region:                'ap-northeast-1',
       path_style:            true,
   }
