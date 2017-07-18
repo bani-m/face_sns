5479b2d9833916611888b56962c505c54f70a3ba
Apple <apple@Apple-no-MacBook-Pro.local>
2017-07-19 00:06:40 +0900

    change storage to fog from file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       provider:              'AWS',
-      #aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      #aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      aws_access_key_id:     "bbb1",
-      aws_secret_access_key: "cccc3",
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region:                'ap-northeast-1',
       path_style:            true,
   }
