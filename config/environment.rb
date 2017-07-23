593988643ab5466c6e8673f328f5e06cf137db8d
Apple <apple@Apple-no-MacBook-Pro.local>
2017-07-23 20:44:42 +0900

    aws

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
-
+require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!# Load the Rails application.
-require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 #Rails.application.initialize!
