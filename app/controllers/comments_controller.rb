d241860b8d6176c032a5a54dab35ca8489340ec5
Apple <apple@Apple-no-MacBook-Pro.local>
2017-07-30 23:28:53 +0900

    pu

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
   def edit
     @comment = Comment.find(params[:id])
     @topic = @comment.topic
-    redirect_to edit_topic_comment_path(@topic)
+    redirect_to edit_topic_comment_path(@comment)
   end
 
   def update
