f330394f99666da47bc9b80e263ec21f7a0bdcb2
Apple <apple@Apple-no-MacBook-Pro.local>
2017-08-21 04:41:53 +0900

    padding-css

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :conversation
   belongs_to :user
-  validates :message, presence: true
   validates_presence_of :body, :conversation_id, :user_id
   def message_time
     created_at.strftime("%m/%d/%y at %l:%M %p")
