25c5dac5e7d370a037fbd803d13838d299aa9ad6
Apple <apple@Apple-no-MacBook-Pro.local>
2017-08-21 04:43:43 +0900

    padding-css

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,7 +27,7 @@ end
 def create
   @message = @conversation.messages.build(message_params)
   if @message.save
-  redirect_to conversation_messages_path(@conversation) notice: "メッセージを作成しました！"
+  redirect_to conversation_messages_path(@conversation)
   end
 end
 private
