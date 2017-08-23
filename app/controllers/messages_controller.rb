c2713956e00055794ad4d5ac7a7c7de4c2321ac5
Apple <apple@Apple-no-MacBook-Pro.local>
2017-08-23 22:04:07 +0900

    padding-css

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -30,7 +30,7 @@ def create
   if @message.save
     redirect_to conversation_messages_path(@conversation)
   else
-    redirect_to conversation_messages_path(@conversation)
+    new_conversation_message
 end
 
 
