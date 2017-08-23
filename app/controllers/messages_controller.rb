b9b9aba9734abb9dd2bb5d5404fcd11cf309ec70
Apple <apple@Apple-no-MacBook-Pro.local>
2017-08-23 22:54:49 +0900

    padding-css

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,43 +1,57 @@
 class MessagesController < ApplicationController
-  before_action do
-    @conversation = Conversation.find(params[:conversation_id])
-  end
+ before_action do
+   @conversation = Conversation.find(params[:conversation_id])
+ end
 
 
-  def index
-  @messages = @conversation.messages
+def index
+   @messages = @conversation.messages
   if @messages.length > 10
-    @over_ten = true
-    @messages = @messages[-10..-1]
+   @over_ten = true
+   @messages = @messages[-10..-1]
   end
 
   if params[:m]
-    @over_ten = false
-    @messages = @conversation.messages
+  @over_ten = false
+  @messages = @conversation.messages
   end
 
   if @messages.last
-    if @messages.last.user_id != current_user.id
-     @messages.last.read = true
-    end
+  if @messages.last.user_id != current_user.id
+  @messages.last.read = true
+  end
   end
 
   @message = @conversation.messages.build
-end
+  end
 
 def create
   @message = @conversation.messages.build(message_params)
-  if @message.save
-    redirect_to conversation_messages_path(@conversation)
+ redirect_to conversation_messages_path(@conversation)
   else
-    redirect_to new_conversation_message_path
+   @messages = Message.where(conversation_id: @conversation.id)
+  if @messages.length > 10
+   @over_ten = true
+   @messages = @messages[-10..-1]
+  end
+
+ if params[:m]
+  @over_ten = false
+  @messages = @conversation.messages
 end
 
+ if @messages.last
+   if @messages.last.user_id != current_user.id
+  @messages.last.read = true
+  end
+end
+render :index
+end
 end
 
 private
-  def message_params
-    params.require(:message).permit(:body, :user_id)
-  end
+def message_params
+params.require(:message).permit(:body, :user_id)
+end
 
 end
